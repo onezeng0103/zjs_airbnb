fdefcc68e8921407f5cbffc0435afd5a19d98230
曾健生 <zjs1780706@163.com>
2024-09-27 17:48:42 +0800

    fix

--- src/components/header/components/left/index.jsx
+++ src/components/header/components/left/index.jsx
@@ -1,8 +1,12 @@
 import React, { memo } from 'react'
 import { LeftWrapper } from './style/index.js'
-
+import IconLogo from '@/assets/svg/iconLogo.jsx'
 const HeaderLeft = memo(() => {
-    return <LeftWrapper>HeaderLeft</LeftWrapper>
+    return (
+        <LeftWrapper>
+            <IconLogo />
+        </LeftWrapper>
+    )
 })
 
 export default HeaderLeft
